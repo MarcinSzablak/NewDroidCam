37f587337d1eff69c80ffb7d9664b3bb27831e03
Marcin Szablak <m.szablak30@gmail.com>
2024-06-20 16:40:37 +0200

    first commit add toolbar add camera preview view add camere flip function

---
+++ app/src/main/java/com/example/mydroidcam/MainActivity.kt
@@ -0,0 +1,160 @@
+package com.example.mydroidcam
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Preview
+import androidx.camera.view.PreviewView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.MaterialToolbar
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    //things related to top bar
+    private lateinit var topAppBar: MaterialToolbar
+
+    //things related to camera
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraPreview: PreviewView
+    private lateinit var defaultCameraSelector: CameraSelector
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var preview: Preview
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //connect objects to view
+        cameraPreview = findViewById(R.id.camera_preview)
+        defaultCameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        topAppBar = findViewById(R.id.top_app_bar)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions()
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.flip_camera ->{
+                    changeCamera()
+                    true
+                }
+
+                else -> { false }
+            }
+
+        }
+    }
+
+    private fun captureVideo() {}
+
+    private fun changeCamera() {
+        if(defaultCameraSelector == CameraSelector.DEFAULT_BACK_CAMERA){
+            //set camera to back one
+            defaultCameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+        }
+        else{
+            //set camera to front one
+            defaultCameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        }
+        bindCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            cameraProvider = cameraProviderFuture.get()
+            // Preview
+            preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(cameraPreview.surfaceProvider)
+                }
+            bindCamera()
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindCamera(){
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll()
+
+            // Bind use cases to camera
+            cameraProvider.bindToLifecycle(
+                this, defaultCameraSelector, preview)
+
+        } catch(exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+}
